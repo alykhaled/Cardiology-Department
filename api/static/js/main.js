dc4f8a46a04411d9bd3dbc71f87fb275260d7a23
Aly Khaled <alykhaled2001@live.com>
2022-01-06 19:30:12 +0200

    Make navbar more responsive

--- api/static/js/main.js
+++ api/static/js/main.js
@@ -3,17 +3,34 @@ var operationsIcon = document.getElementById("operationsIcon");
 var patientsIcon = document.getElementById("patientsIcon");
 var nursesIcon = document.getElementById("nursesIcon");
 var roomsIcon = document.getElementById("roomsIcon");
+var menuBtn = document.getElementById("menuBtn");
+var mainContent = document.getElementById("mainContent");
 roomsIcon.classList.add('selected');
 
+var isMenuOpen = false;
+function openMenu(e)
+{
+    if(isMenuOpen)
+    {
+        isMenuOpen = false;
+        myNavbar.classList.remove('open');        
+    }
+    else
+    {
+        isMenuOpen = true;
+        myNavbar.classList.add('open');  
+        console.log("test")      
+    }
+}
 
-// function chooseView(e)
-// {
-//     var ul = document.getElementById("navBar");
-//     var items = ul.getElementsByTagName("li");
-//     for (var i = 0; i < items.length; ++i) {
-//         console.log(items[i].classList);
-//         items[i].classList.remove('selected');
-//         console.log(items[i].classList);
-//     }
-//     e.classList.add('selected');
-// }
+function chooseView(e)
+{
+    var ul = document.getElementById("navBar");
+    var items = ul.getElementsByTagName("li");
+    for (var i = 0; i < items.length; ++i) {
+        console.log(items[i].classList);
+        items[i].classList.remove('selected');
+        console.log(items[i].classList);
+    }
+    e.classList.add('selected');
+}
